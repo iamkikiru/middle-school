2c5ecf13fb102de83532946fe06fd7174260450b
iamkikiru <45275660+iamkikiru@users.noreply.github.com>
2018-11-23 11:10:02 +0900

    Add files via upload

---
+++ Programmer/Programming_v1/Programming_v1.ino
@@ -0,0 +1,85 @@
+
+#define NOTE_G4 392
+#define NOTE_C5 523
+
+int yellowLed = 8;
+int greenLed = 9;
+int piezoPin = 10;
+int yellowButton = 11;
+int greenButton = 12;
+
+int yellowButtonState  = 1; //안눌렸을때 1
+int greenButtonState  = 1;  //안눌렸을때 1
+
+void setup() {
+
+  // led OUTPUT 셋업
+  pinMode(yellowLed,OUTPUT);
+  pinMode(greenLed,OUTPUT);
+  
+  // piezoPin OUTPUT 셋업
+  pinMode(piezoPin,OUTPUT);
+  
+  // button INPUT_PULLUP 셋업
+  pinMode(yellowButton,INPUT_PULLUP);
+  pinMode(greenButton,INPUT_PULLUP);
+
+  delay(500);
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+
+void playTone( int frequency , int duration){
+  tone(piezoPin, frequency, duration);
+  delay(duration);
+}
+
+///////////////////////실습 : green 버튼 누루면 green led 켜지고 소리내기 ///////////////////////////
+
+void loop() {
+  
+  yellowButtonState = digitalRead(yellowButton);  //버튼핀의 값을 읽어서...
+  if(yellowButtonState == 0){   //값이 0(버튼이 눌러졌으면)
+     digitalWrite(yellowLed,HIGH);    //yellowLed를 켜고
+     playTone(NOTE_G4,100);           // 0.1초간 소리내고
+     delay(100);                      // 소리날동안 기다리고
+     digitalWrite(yellowLed,LOW);     // yellowLed 끄고
+     delay(100);                      // 0.1초 기다린다.
+  }
+ greenButtonState = digitalRead(greenButton);  //버튼핀의 값을 읽어서...
+  if(greenButtonState == 0){   //값이 0(버튼이 눌러졌으면)
+     digitalWrite(greenLed,HIGH);    //yellowLed를 켜고
+     playTone(NOTE_C5,100);           // 0.1초간 소리내고
+     delay(100);                      // 소리날동안 기다리고
+     digitalWrite(greenLed,LOW);     // yellowLed 끄고
+     delay(100);                      // 0.1초 기다린다.
+  }
+
+//미션 1 : greenButton, NOTE_C5
+
+
+}
+
+
+
+
+
+/*
+  greenButtonState = digitalRead(greenButton);
+  if(greenButtonState == 0){   //버튼이 눌러졌으면
+     digitalWrite(greenLed,HIGH);
+     playTone(NOTE_C5,100);
+     delay(100);
+     digitalWrite(greenLed,LOW);
+     delay(100);
+  }
+*/
+
+
+
+
+
+
+
+
---
+++ Programmer/Programming_v3/Programming_v3.ino
@@ -0,0 +1,115 @@
+
+#define NOTE_C4 262
+#define NOTE_E4 330
+#define NOTE_F4 349
+#define NOTE_G4 392
+#define NOTE_C5 523
+
+#define NOTE_C3 131
+#define NOTE_G3 196
+
+int yellowLed = 8;
+int greenLed = 9;
+int piezoPin = 10;
+int yellowButton = 11;
+int greenButton = 12;
+
+int yellowButtonState  = 1; //안눌렸을때 1
+int greenButtonState  = 1;  //안눌렸을때 1
+
+
+void setup() {
+
+  // led OUTPUT 셋업
+  pinMode(yellowLed,OUTPUT);
+  pinMode(greenLed,OUTPUT);
+  
+  // piezoPin OUTPUT 셋업
+  pinMode(piezoPin,OUTPUT);
+  
+  // button INPUT_PULLUP 셋업
+  pinMode(yellowButton,INPUT_PULLUP);
+  pinMode(greenButton,INPUT_PULLUP);
+
+  delay(500);
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+
+void playStartMelody(){
+  int startMelody[] = {NOTE_C4, NOTE_F4, NOTE_C4, NOTE_F4, NOTE_C4, NOTE_F4,
+                     NOTE_C4, NOTE_F4, NOTE_G4, NOTE_F4, NOTE_E4, NOTE_F4, NOTE_G4};
+  int startDuration[] = {100,   200,     100,     200,     100,      400,
+                       100,   100,     100,     100,      200,     100,    500};
+                       
+  for(int i=0; i<13; i++){
+    if(i%2 == 0 ){
+      digitalWrite(yellowLed,HIGH);
+    }
+    if(i%2 == 1){
+      digitalWrite(greenLed,HIGH);
+    }
+
+    playTone(startMelody[i],startDuration[i]);
+    ledOff();  
+    delay(25);
+  }
+}
+
+void ledOff(){
+  digitalWrite(yellowLed,LOW);
+  digitalWrite(greenLed,LOW);
+}
+void ledOn(){
+  digitalWrite(yellowLed,HIGH);
+  digitalWrite(greenLed,HIGH);
+}
+void playTone( int frequency , int duration){
+  tone(piezoPin, frequency, duration);
+  delay(duration);
+}
+
+void pushButton(){
+  int beeps[] = {NOTE_G3, NOTE_C3};
+  int duration = 200;
+  
+  for(int i=0; i<3; i++){
+    ledOn();
+    playTone(beeps[0],duration);
+    ledOff();
+    playTone(beeps[1],duration);
+  }
+  delay(500);
+}
+
+void loop() {
+  
+  playStartMelody();
+  
+  int buttonState = 1;
+
+  while(buttonState == 1) {
+      yellowButtonState = digitalRead(yellowButton);
+      greenButtonState = digitalRead(greenButton);
+      if(yellowButtonState == 0 || greenButtonState == 0) {
+          pushButton();
+          delay(1000);
+          buttonState = 0;
+      }
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
