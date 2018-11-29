64a7d17f21f481e5e450bb28f2d23738baa1bea4
iamkikiru <45275660+iamkikiru@users.noreply.github.com>
2018-11-29 13:34:37 +0900

    Add files via upload

---
+++ Programming_v11/Programming_v11.ino
@@ -0,0 +1,75 @@
+
+#define NOTE_G4 392
+#define NOTE_C5 523
+
+int redLed = 8;
+int activePiezo = 9;
+int redButton = 10;
+int yellowLed = 11;
+int passivePiezo = 12;
+
+int redButtonState  = 1; //안눌렸을때 1
+
+void setup() {
+
+  // led OUTPUT 셋업
+  pinMode(redLed,OUTPUT);
+  pinMode(yellowLed,OUTPUT);
+  
+  // piezoPin OUTPUT 셋업
+  pinMode(activePiezo,OUTPUT);
+  pinMode(passivePiezo,OUTPUT);
+  
+  // button INPUT_PULLUP 셋업
+  pinMode(redButton,INPUT_PULLUP);
+
+  delay(500);
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+
+void playTone( int frequency , int duration){
+  tone(passivePiezo, frequency, duration);
+  delay(duration);
+}
+
+///////////////////////실습 : red 버튼 누루면 red led 켜지고 소리내기 ///////////////////////////
+
+void loop() {
+  
+  redButtonState = digitalRead(redButton);  //버튼핀의 값을 읽어서...
+  if(redButtonState == 0){   //값이 0(버튼이 눌러졌으면)
+     digitalWrite(redLed,HIGH);    //redLed를 켜고
+     digitalWrite(activePiezo,HIGH);
+     //playTone(NOTE_G4,100);           // 0.1초간 소리내고
+     delay(100);                      // 소리날동안 기다리고
+     digitalWrite(redLed,LOW);     // redLed 끄고
+     digitalWrite(activePiezo,LOW);
+     delay(100);                      // 0.1초 기다린다.
+  }
+
+}
+
+
+
+
+
+/*
+  greenButtonState = digitalRead(greenButton);
+  if(greenButtonState == 0){   //버튼이 눌러졌으면
+     digitalWrite(greenLed,HIGH);
+     playTone(NOTE_C5,100);
+     delay(100);
+     digitalWrite(greenLed,LOW);
+     delay(100);
+  }
+*/
+
+
+
+
+
+
+
+
---
+++ Programming_v13/Programming_v13.ino
@@ -0,0 +1,112 @@
+
+#define NOTE_C4 262
+#define NOTE_E4 330
+#define NOTE_F4 349
+#define NOTE_G4 392
+#define NOTE_C5 523
+
+#define NOTE_C3 131
+#define NOTE_G3 196
+
+int redLed = 8;
+int activeBuzzer = 9;
+int redButton = 10;
+int yellowLed = 11;
+int passiveBuzzer = 12;
+
+int redButtonState  = 1; //안눌렸을때 1
+
+
+void setup() {
+
+  // led OUTPUT 셋업
+  pinMode(redLed,OUTPUT);
+  pinMode(yellowLed,OUTPUT);
+  
+  // piezoPin OUTPUT 셋업
+  pinMode(activeBuzzer,OUTPUT);
+  pinMode(passiveBuzzer,OUTPUT);
+  
+  // button INPUT_PULLUP 셋업
+  pinMode(redButton,INPUT_PULLUP);
+
+  delay(500);
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+
+void playStartMelody(){
+  int startMelody[] = {NOTE_C4, NOTE_F4, NOTE_C4, NOTE_F4, NOTE_C4, NOTE_F4,
+                       NOTE_C4, NOTE_F4, NOTE_G4, NOTE_F4, NOTE_E4, NOTE_F4, NOTE_G4};
+  int startDuration[] = {100,   200,     100,     200,     100,      400,
+                         100,   100,     100,     100,      200,     100,    500};
+                       
+  for(int i=0; i<13; i++){
+    if(i%2 == 0 ){
+      digitalWrite(redLed,HIGH);
+    }
+    if(i%2 == 1){
+      digitalWrite(yellowLed,HIGH);
+    }
+
+    playTone(startMelody[i],startDuration[i]);
+    ledOff();  
+    delay(25);
+  }
+}
+
+void ledOff(){
+  digitalWrite(redLed,LOW);
+  digitalWrite(yellowLed,LOW);
+}
+void ledOn(){
+  digitalWrite(redLed,HIGH);
+  digitalWrite(yellowLed,HIGH);
+}
+void playTone( int frequency , int duration){
+  tone(passiveBuzzer, frequency, duration);
+  delay(duration);
+}
+
+void pushButton(){
+  int beeps[] = {NOTE_G3, NOTE_C3};
+  int duration = 200;
+  
+  for(int i=0; i<3; i++){
+    ledOn();
+    playTone(beeps[0],duration);
+    ledOff();
+    playTone(beeps[1],duration);
+  }
+  delay(500);
+}
+
+void loop() {
+  playStartMelody();
+
+  int buttonState = 1;
+
+  while(buttonState == 1) {
+      redButtonState = digitalRead(redButton);
+      if(redButtonState == 0) {
+          pushButton();
+          delay(1000);
+          buttonState = 0;
+      }
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
